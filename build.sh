4fddcf641a6fec8c2de7be6eb8b8593369ead048
Baljeesi <150318997+Baljeesi@users.noreply.github.com>
2025-10-22 19:51:19 +0530

    build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cd /home/ubuntu/Udemy-section7/
+cd /home/ubuntu/Course-section7-main/
 npm install
 npm run build
 chmod 744 push_docker_image.sh
-/home/ubuntu/Udemy-section7/push_docker_image.sh
+/home/ubuntu/Course-section7-main/push_docker_image.sh
