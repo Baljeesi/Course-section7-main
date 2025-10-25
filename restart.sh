1a35a46329e347e6bff71c685ec3adaa26d325d3
Baljeesi <150318997+Baljeesi@users.noreply.github.com>
2025-10-25 13:26:25 +0530

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-ssh ubuntu@172.31.23.70  "microk8s kubectl rollout restart deployment react-deployment -n react-microk8s" ##update the AWS private/public IP
+ssh ubuntu@172.31.23.70  "microk8s kubectl rollout restart deployment react-deployment -n react-microk8s" ##update the AWS aprivate/public IP
 
